a473efa8091835139ae8f8baab96d2ee16d760e1
Md Abushama <mohammedabushamacom@gmail.com>
2024-03-06 21:34:16 +0530

    all files commit

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+ const asyncHandler = (fn)=>{
+    (req ,res ,next) => {
+        Promise.resolve(fn(req,res,next).
+          catch((err) => next(err))
+        )
+    }
+ }
+
+
+//export {asyncHandler}
+
+// const asyncHandler = (fn) => async(req ,res ,next) =>{
+
+//     try{
+//           await fn(req ,res,next)
+//     }
+//     catch (error) {
+//         res.status(err.code || 500).json({
+//             sueccess:false ,
+//             message:err.message
+//         })
+//     }
+// }
